dcc76fdee9467c7824b63019fd1c81b54a490d9b
BuildTools <unconfigured@null.spigotmc.org>
2021-02-12 14:51:20 -0800

    BagInterface1.0

--- BagInterface.java
+++ BagInterface.java
@@ -1,3 +1,28 @@
-public interface BagInterface{
-    
+public interface BagInterface<T>
+{
+
+    public int getCurrentSize();
+
+    public boolean isEmpty();
+
+    public boolean add(T newEntry);
+
+    public T remove();
+
+    public boolean remove(T anEntry);
+
+    public void clear();
+
+    public int getFrequencyOf(T anEntry);
+
+    public boolean contains(T anEntry);
+
+    public T[] toArray();
+
+    public BagInterface<T> union(BagInterface<T> inputBag);
+
+    public BagInterface<T> intersection(BagInterface<T> inputBag);
+
+    public BagInterface<T> difference(BagInterface<T> inputBag);
+
 }
